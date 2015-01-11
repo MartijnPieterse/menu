cf3392dccd8dc0d7d282ca5e422d37c955f4707e
MartijnPieterse <martijn.pieterse@grondklont.nl>
2015-01-11 19:26:04 +0100

    Working version

--- menu.py
+++ menu.py
@@ -1,25 +1,44 @@
 class Menu:
-    def __init__(self):
+    def __init__(self, description):
         self.options = []
         self.name = "Menu"
         self.options = []
-
-    def setname(self, description):
+        self.lastchoice = -1
         self.name = description
 
     def addoption(self, description, f):
         assert(isinstance(description, str))
-        self.options.append(description)
+        assert(len(self.options) < 9)
+        self.options.append([description, f])
 
     def start(self):
-        print "-" * len(self.name)
-        print self.name
-        print "-" * len(self.name)
+        while True:
+            print "-" * len(self.name)
+            print self.name
+            print "-" * len(self.name)
 
-        i = 1
-        for o in self.options:
-            print " [%d] - %s" % (i, o)
-            i += 1
+            i = 1
+            for o in self.options:
+                print "[%d] - %s" % (i, o[0])
+                i += 1
 
-        print
-        print " [0] - Back"
+            print
+            print "[0] - Back"
+
+            c = raw_input("Choice [%d]: " % (self.lastchoice+1))
+
+            if len(c) == 0:
+                ci = self.lastchoice
+            else:
+                ci = int(c)-1
+
+            if ci == -1:
+                self.lastchoise = -1
+                return
+
+            if ci < 0 or ci >= len(self.options):
+                continue
+
+            self.lastchoice = ci
+
+            self.options[ci][1]()
--- test.py
+++ test.py
@@ -1,10 +1,17 @@
 from menu import *
 
-m = Menu()
+def f(s):
+    print s
 
-m.addoption("start", None)
-m.addoption("stop", None)
-m.addoption("go", None)
-m.addoption("with", None)
+m = Menu("Startmenu")
+m.addoption("start", lambda:f("start"))
+m.addoption("stop", lambda:f("stop"))
+m.addoption("go", lambda:f("go"))
+m.addoption("with", lambda:f("with"))
+
+m2 = Menu("Submenu")
+m2.addoption("alleen deze", lambda:f("x"))
+
+m.addoption("submenu", m2.start)
 
 m.start()
